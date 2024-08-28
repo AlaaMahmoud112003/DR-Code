70948bd5d509d18d3cc486028c2bdd3a671e0d6f
Alaa Mahmoud <137706424+AlaaMahmoud112003@users.noreply.github.com>
2024-08-28 06:20:58 +0300

    Add files via upload

---
+++ src/components/common/header/Head.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className="container flexSB">
+          <div className="logo">
+            <h1>DR Code</h1>
+            <span> ONLINE EDUCATION & LEARNING</span>
+          </div>
+          <div className="social">
+            <i className="fab fa-facebook-f"></i>
+            <i className="fa fa-instagram"></i>
+            <i className='fab fa-twitter icon'></i>
+            <i className='fab fa-youtube icon'></i>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
